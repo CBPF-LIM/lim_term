e2ec6dd5d7c48bda9520702f6fa951f8005d6e74
David Vaz Teixeira <dvt4qb@gmail.com>
2025-07-28 02:07:50 -0300

    remove port simulation and added a data simulation without using serial port

--- limterm/utils/__init__.py
+++ limterm/utils/__init__.py
@@ -1,5 +1,11 @@
 from .serial_utils import SerialPortManager, DataParser
 from .file_utils import FileManager
-from .mock_serial import MockSerial
+from .mock_serial import MockSerial, SyntheticDataGenerator
 
-__all__ = ["SerialPortManager", "DataParser", "FileManager", "MockSerial"]
+__all__ = [
+    "SerialPortManager",
+    "DataParser",
+    "FileManager",
+    "MockSerial",
+    "SyntheticDataGenerator",
+]
